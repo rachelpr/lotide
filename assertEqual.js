ab28dbc72078521606e3265cbf7f5bb22351729e
Rachel PR <rplourder@gmail.com>
2022-01-26 19:21:10 -0400

    Learning about console.assert

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😖😖😖 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Frankie", "Frankie");
+assertEqual(1, 5);
