48fe48e70bd49bcb405eb3b4074220374c7b26f7
Rachel PR <rplourder@gmail.com>
2022-01-28 14:31:07 -0400

    Updated code

--- assertEqArray.js
+++ assertEqArray.js
@@ -1,19 +1,5 @@
-// assertEqual function that will test if two objects are equal
-const assertEqual = function (actual, expected) {
-  //this checks if actual is the same as expected
-  if (actual === expected) {
-    //outputs they are the same
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-    // if they aren't the same
-  } else {
-    //tells us they aren't
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
 
-};
-
-
-const eqArrays = function (arr1, arr2) {
+const eqArray = function (arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -21,7 +7,18 @@ const eqArrays = function (arr1, arr2) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+
+const assertEqArray = function (arr1, arr2) {
+  if (eqArray(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+assertEqArray([1, 2, 3], [1, 2, 3])
+assertEqArray([1, 2, 3], [3, 2, 1]); // => false
+assertEqArray(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertEqArray(["1", "2", "3"], ["1", "2", 3]); // => false
