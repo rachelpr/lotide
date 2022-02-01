428e61eee17c69c02c0784a2dff9ee9909e1fc98
Rachel PR <rplourder@gmail.com>
2022-02-01 19:27:00 -0400

    A function to find the position of each letter in a string

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+
+const eqArray = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArray(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+};
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i)
+    } else {
+      results[sentence[i]] = [i]
+    }
+
+  }
+  return results;
+}
+
+//console.log(letterPositions("lighthouse"))
+
+
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
