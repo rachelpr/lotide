0da6833e8653d2519e9c10016a95566f6d231d42
Rachel PR <rplourder@gmail.com>
2022-02-03 21:24:12 -0400

    Some changes added

--- takeUntil.js
+++ takeUntil.js
@@ -26,8 +26,8 @@ const takeUntil = function (array, callback) {
   // console.log('array: ', array);
   // console.log('callback: ', callback);
   for (let item of array) {
-    // console.log('item BEFORE: ', item);
-    // console.log('item AFTER: ', callback(item));
+    // console.log('Item: ', item);
+    // console.log('Callback: ', callback(item));
     // console.log('---');
     if (callback(item) !== true) {
       results.push(item)
