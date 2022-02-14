bf6d5635e14b64edb72cbc3009ada327415a8773
Rachel PR <rplourder@gmail.com>
2022-02-14 19:48:25 -0400

    Updating and exporting middle function

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArray = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArray(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-
-};
 
 //take an array and return the middle-most element
 const middle = function (arr1) {
@@ -36,17 +16,4 @@ const middle = function (arr1) {
 };
 
 
-//TEST CODE
-
-//assertArraysEqual(middle([1], [])) // => []
-// console.log(middle([1, 2])) // => []
-// console.log(middle([1, 2, 3])) // => [2]
-// console.log(middle([1, 2, 3, 4, 5])) // => [3]
-// console.log(middle([1, 2, 3, 4])) // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-
-const words = ["hello", "lighthouse", "labs", "is", "cool"];
-let middleWord = middle(words)
-
-assertArraysEqual(middleWord, ["labs"]) //passes
-assertArraysEqual(middleWord, ["lighthouse"]) //fails
+module.exports = middle
