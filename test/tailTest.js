a323f630883b3f1e4cb98e2b03029f7922208f3d
Rachel PR <rplourder@gmail.com>
2022-02-14 21:16:17 -0400

    Updating code to use mocha/chai tests

---
+++ test/tailTest.js
@@ -0,0 +1,21 @@
+const assert = require("chai").assert;
+const tail = require("../tail")
+
+describe("#tail", () => {
+  it("returns '[Lighthouse, Labs]' for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), [])
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), [])
+  })
+})
+
+
+// const result = tail([1]);
+// assertEqual(result.length[1])
+// assertEqual(result[0], []);
+
+
