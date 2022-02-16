b672004a197fd7e32724addd0cf5aba6ddde9a9f
Rachel PR <rplourder@gmail.com>
2022-02-15 22:07:42 -0400

    Updating code and exporting the function

--- without.js
+++ without.js
@@ -1,23 +1,3 @@
-const eqArray = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArray(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-
-};
 
 const without = function (arr1, arr2) {
   let newArray = [];
@@ -35,25 +15,4 @@ const without = function (arr1, arr2) {
   return newArray
 };
 
-//Testing without function
-//console.log(without([1, 2, 3], [1])) // => [2, 3]
-//console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-
-//Testing if arrays are empty
-const words = ["hello", "world", "lighthouse"];
-
-//Testing no words are removed - this passes
-let withoutWord = without(words, []);
-assertArraysEqual(withoutWord, ["hello", "world", "lighthouse"]);
-
-//Testing when all words are removed, a.k.a two empty arrays - this passes
-withoutWord = without(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(withoutWord, []);
-
-//Testing with words not in words, so no words removed - this passes
-withoutWord = without(words, ["apples", "bananas"]);
-assertArraysEqual(withoutWord, ["hello", "world", "lighthouse"]);
-
-//Testing with removing a word - this fails
-withoutWord = without(words, ["hello"]);
-assertArraysEqual(withoutWord, ["hello", "world", "lighthouse"]);
+module.exports = without
