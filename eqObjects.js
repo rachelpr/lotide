cfc19a9e0d567716e93a2c8951bcb0c2a4266eeb
Rachel PR <rplourder@gmail.com>
2022-02-15 20:08:52 -0400

    Updating some coe and exporting the function

--- eqObjects.js
+++ eqObjects.js
@@ -1,28 +1,4 @@
-//a function to check if arrays are equal
-const eqArray = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// assertEqual function that will test if two objects are equal
-const assertEqual = function (actual, expected) {
-  //this checks if actual is the same as expected
-  if (actual === expected) {
-    //outputs they are the same
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-    // if they aren't the same
-  } else {
-    //tells us they aren't
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require("./eqArrays");
 
 //a function to compare two objects
 const eqObjects = function (object1, object2) {
@@ -38,7 +14,7 @@ const eqObjects = function (object1, object2) {
   for (const key of keys1) {
     //compare the values for object1 and object2 are the same (checking type too)
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      return eqArray(object1[key], object2[key])
+      return eqArrays(object1[key], object2[key])
     } else if (object1[key] === object2[key]) {
     } else {
       return false;
@@ -48,17 +24,4 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
-//TEST CODE
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" }
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+module.exports = eqObjects
