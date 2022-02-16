d3b1109f99a1105bc482163f7ab9f458f3a0277e
Rachel PR <rplourder@gmail.com>
2022-02-15 20:47:16 -0400

    Updating code and exporting the function

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,4 @@
 
-const eqArray = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArray(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-
-};
-
 const letterPositions = function (sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -33,7 +12,4 @@ const letterPositions = function (sentence) {
   return results;
 }
 
-//console.log(letterPositions("lighthouse"))
-
-
-assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
+module.exports = letterPositions
