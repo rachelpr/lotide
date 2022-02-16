f913dfe3c629cfd29aac4c11b39ef78a893dc7cd
Rachel PR <rplourder@gmail.com>
2022-02-15 20:36:49 -0400

    updating code and exporting the function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,15 +1,3 @@
-// assertEqual function that will test if two objects are equal
-const assertEqual = function (actual, expected) {
-  //this checks if actual is the same as expected
-  if (actual === expected) {
-    //outputs they are the same
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-    // if they aren't the same
-  } else {
-    //tells us they aren't
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const findKeyByValue = function (object, value) {
   //scan the object and return the first key which contains the given value
@@ -22,20 +10,7 @@ const findKeyByValue = function (object, value) {
   return undefined;
 };
 
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
-console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
-console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue
 
 
 
