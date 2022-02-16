a38d3f31d21f30e4a93e641a9ef150d669cf48e4
Rachel PR <rplourder@gmail.com>
2022-02-15 20:38:35 -0400

    Updating code and exporting function

--- flatten.js
+++ flatten.js
@@ -1,20 +1,3 @@
-const eqArray = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArray(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-
-};
 
 const flatten = function (arr1) {
   let flattened = []
@@ -31,4 +14,4 @@ const flatten = function (arr1) {
   return flattened
 }
 
-console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
+module.exports = flatten
