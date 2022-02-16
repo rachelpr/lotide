969976064f2ea3489db61a562cfbcb8da81c63ca
Rachel PR <rplourder@gmail.com>
2022-02-15 22:05:49 -0400

    Updating code and exporting the function

--- takeUntil.js
+++ takeUntil.js
@@ -1,23 +1,3 @@
-const eqArray = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArray(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-
-};
 
 //will take two parameters - an array and callback
 //return a slice of the array until the callback returns truthy
@@ -38,18 +18,4 @@ const takeUntil = function (array, callback) {
   return results;
 };
 
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-//console.log(results1);
-
-//console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-//console.log(results2);
-
-assertArraysEqual(results1, [1, 2, 5, 7, 2])
-assertArraysEqual(results1, [1, 2, 5, 7, -1])
-assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"])
-assertArraysEqual(results2, ["I've", "been", "to", "Redwood"])
+module.exports = takeUntil
